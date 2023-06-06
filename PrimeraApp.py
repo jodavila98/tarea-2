80c6295ecbeabad049ede6a755778d3a090f0897
jodavila98 <ajdavila@espol.edu.ec>
2023-06-05 23:30:06 -0500

    First commit.

---
+++ PrimeraApp.py
@@ -0,0 +1,9 @@
+import streamlit as st
+#Agregar título de la aplicación web
+st.title("Welcome to software pretroleum engineer")
+
+#Agregar información
+st.write("This is going to be used as the kickoff lecture of this subject")
+
+#Línea imaginaria
+st.markdown("---")
